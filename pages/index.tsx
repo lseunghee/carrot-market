a1cd1fd9dfb3e649772d0e6023efb546eaef5865
lseunghee <43093660+lseunghee@users.noreply.github.com>
2022-12-16 02:18:09 +0900

    tailwindcss install ...

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,3 +1,11 @@
-export default function Home() {
-  return null;
-}
+import { NextPage } from "next";
+
+const Home: NextPage = () => {
+  return (
+    <div className="bg-red-200">
+      <h1 className="text-blue-500">it works</h1>
+    </div>
+  );
+};
+
+export default Home;
